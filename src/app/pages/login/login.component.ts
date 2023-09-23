c32be9067aa69b451f3711a70a482ef05fb004d2
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-09-23 16:53:06 -0300

    update form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormValidators } from '../../shared/form-validators';
+import { FormBaseComponent } from '../../shared/form-base.component';
+import { O } from '@fullcalendar/core/internal-common';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent 
+  extends FormBaseComponent 
+  implements OnInit
+  {
+    constructor(private fb: FormBuilder) {
+      super();
+  }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required]],
+
+    });
+  }
+  submit() {
+    let valueSubmit = Object.assign({}, this.form.value);
+    console.log(valueSubmit);
+
+    // TODO: Add user to database
+  }
 
 }
