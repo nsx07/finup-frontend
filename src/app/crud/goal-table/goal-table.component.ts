8b9d59d9293176bd265be0e67cd1a8ca9b87925a
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-11-09 20:54:01 -0300

    [add]: delete

--- src/app/crud/goal-table/goal-table.component.ts
+++ src/app/crud/goal-table/goal-table.component.ts
@@ -12,10 +12,30 @@ export class GoalTableComponent {
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.fetchGoals();
+  }
+
+  fetchGoals(): void {
     this.apiService.requestFromApi("goal/getAll").subscribe({
       next: (data) => {
         this.goals = data;
       },
     });
+  }
+
+  onDelete(selectedGoal: any): void {
+    if (confirm("Are you sure you want to delete this goal?")) {
+      this.apiService
+        .deleteFromApi("goal/delete/" + selectedGoal.id)
+        .subscribe({
+          next: () => {
+            console.log("Goal deleted successfully");
+            this.fetchGoals(); // Atualiza a lista após a deleção
+          },
+          error: (error) => {
+            console.error("Error deleting goal:", error);
+          },
+        });
+    }
   }
 }
