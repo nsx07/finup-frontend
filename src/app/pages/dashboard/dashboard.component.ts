9108760b0566eb00fa644495cb883ed2f3899ffe
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-11-12 18:34:36 -0300

    redirect Buttons

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api-service.service';
 import { CardModule } from 'primeng/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,5 +10,10 @@ import { CardModule } from 'primeng/card';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  constructor(private router: Router) { }
+
+  redirectToPage(pageName: string) {
+    this.router.navigate([`/${pageName}`]);
+  }
 
 }
